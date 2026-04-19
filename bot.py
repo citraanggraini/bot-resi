55fc130caf451c7655eb88e479bf90477880a622
citraanggraini <citraanggraini23z3@gmail.com>
2026-04-20 05:39:05 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,61 +1,13 @@
-import os
-import requests
 from telegram import Update
-from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
-TOKEN = os.getenv("TOKEN")
-API_KEY = os.getenv("API_KEY")
+TOKEN ="8675610533:AAHANBzZHAA9VBzgPGNW4SxWKsBSJV9z2GU"
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text("Kirim nomor resi J&T kamu")
-
-async def cek_resi(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    resi = update.message.text.strip().upper()
-
-    url = f"https://api.binderbyte.com/v1/track?api_key={API_KEY}&courier=jnt&awb={resi}"
-
-    try:
-        response = requests.get(url).json()
-    except:
-        await update.message.reply_text("❌ Gagal mengambil data")
-        return
-
-    if response.get("status") == 200:
-        data = response["data"]
-        summary = data.get("summary", {})
-        detail = data.get("detail", {})
-        history = data.get("history", [])
-
-        last = history[0] if history else {}
-
-        text = f"""
-📦 EKSPEDISI J&T
-└ J&T Express
-
-📩 Resi
-├ No Resi : {resi}
-└ Service : {summary.get("service", "-")}
-
-📮 Status
-├ {summary.get("status", "-")}
-└ {summary.get("date", "-")}
-
-🚩 Penerima
-├ {detail.get("receiver", "-")}
-└ {detail.get("destination", "-")}
-
-📍 Update Terakhir
-├ Lokasi : {last.get("location", "-")}
-├ Status : {last.get("desc", "-")}
-└ Waktu : {last.get("date", "-")}
-"""
-        await update.message.reply_text(text)
-    else:
-        await update.message.reply_text("❌ Resi tidak ditemukan")
+    await update.message.reply_text("Bot aktif!")
 
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(CommandHandler("start", start))
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, cek_resi))
 
 app.run_polling()
